b7b5d404bd667ad2d060bf379e749172e0bf07d9
Dimitri Aatos Ellinas <d.a.ellinas@gmail.com>
2025-01-26 20:03:23 +0200

    refactor: moving global styles to dedicated css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
       NUXT_PUBLIC_CMS_URI: process.env.NUXT_PUBLIC_CMS_URI,
     },
   },
+  css: ["@/assets/main.css"],
   fonts: {
     families: [{ name: "Satoshi", provider: "fontshare", weights: [300] }],
     defaults: {
