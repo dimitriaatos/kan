cb328c63a47b853a87731c019bc6ade1f65c0159
Dimitri Aatos Ellinas <d.a.ellinas@gmail.com>
2025-02-11 23:20:30 +0200

    feat: fixed nav position

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-    <Nav />
+    <Nav id="nav" />
+    <div style="height: 8em" />
     <slot />
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style scoped>
+#nav {
+  position: fixed;
+  top: 0;
+  left: var(--padding);
+  right: var(--padding);
+  background-color: white;
+}
+</style>
